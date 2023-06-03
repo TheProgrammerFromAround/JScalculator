3c81850629e13aa5fee753d259c903705305edde
TheProgrammerFromAround <coolirsa2ooy@gmail.com>
2023-06-04 02:17:12 +0400

    commiti

--- calculatorLogic.js
+++ calculatorLogic.js
@@ -1,3 +1,32 @@
-function basic(){
-    document.getElementById("accumulator").innerHTML="tf?";
+var gee = "";
+
+function clearButton() {
+    document.getElementById("accumulator").innerHTML = "";
+    gee = "";
+}
+
+function numbers() {
+    //makes array with all buttons
+    var buttons = document.getElementsByClassName("button");
+    var operators = document.getElementsByClassName("operator");
+
+    //loops through 'buttons' array
+    for (var i = 0; i < buttons.length; i++) {
+        //checks which button clicked from array
+        buttons[i].addEventListener("click", function () {
+            //gets the text on button clicked
+            var buttonValue = this.innerHTML;
+            //adds to accumulator
+            document.getElementById("accumulator").innerHTML = buttonValue;
+        })
+    }
+    for (var i = 0; i < operators.length; i++) {
+        //checks which button clicked from array
+        operators[i].addEventListener("click", function () {
+            //gets the text on button clicked
+            var operatorValue = this.innerHTML;
+            //adds to accumulator
+            document.getElementById("accumulator").innerHTML = operatorValue;
+        })
+    }
 }
